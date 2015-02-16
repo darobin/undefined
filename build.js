de403915063db9966aa9f8db108d32ad3c6f9240
Robin Berjon <robin@berjon.com>
2015-02-16 22:17:20 +0100

    building the CSS

--- build.js
+++ build.js
@@ -2,13 +2,15 @@
 
 var fs = require("fs")
 ,   jn = require("path").join
+,   CleanCSS = require("clean-css")
+,   crypto = require("crypto")
 ,   rfs = function (file) { return fs.readFileSync(jn(__dirname, file), "utf8"); }
 ,   wfs = function (file, content) { fs.writeFileSync(jn(__dirname, file), content, "utf8"); }
 ;
 
 // XXX
 // grab each HTML source in turn, and inject it in place
-// build the CSS file, hash it, save to hashed file, insert into HTML
+// build the CSS file, uglify it, hash it, save to hashed file, insert into HTML
 // same for JS
 
 // load up base skeleton
@@ -18,7 +20,21 @@ function loadSkeleton () {
 
 // process CSS
 function processCSS () {
-    return "";
+    var sources = ["node_modules/normalize.css/normalize.css"]
+    ,   css = ""
+    ;
+    sources.forEach(function (file) { css += rfs(file); });
+    var cssmin = new CleanCSS({
+            keepSpecialComments:    0
+        ,   root:                   __dirname
+        ,   relativeTo:             jn(__dirname, "css")
+        ,   processImport:          true
+        ,   noAdvanced:             true
+        }).minify(css)
+    ,   hash = crypto.createHash("md5").update(cssmin.styles, "utf8").digest("hex")
+    ;
+    wfs("css/" + hash + ".min.css", cssmin.styles);
+    return "<link rel='stylesheet' href='/css/" + hash + ".css'>";
 }
 
 // process JS
@@ -36,7 +52,10 @@ function applyTemplates (src) {
             else if (/^include/.test(m1)) {
                 var file = m1.replace(/^include\s+/, "");
                 if (fs.existsSync(jn(__dirname, file))) return rfs(file);
-                else console.error("File not found: " + file);
+                else {
+                    console.error("File not found: " + file);
+                    return "";
+                }
             }
         });
     return hasReplaced ? applyTemplates(res) : res;
