aee25b5c95beeba3837a5f92a4c4615cfaf41314
Robin Berjon <robin@berjon.com>
2015-03-10 15:04:15 +0100

    progress on sticky headers

--- js/sticky.js
+++ js/sticky.js
@@ -14,55 +14,86 @@
 //  - the background thing is handled by *constantly* having said bg element at the top and just
 //    resizing it to match our needs
 
-
 (function ($) {
+    var headers = []
+    ,   $win = $(window)
+    ,   currentIdx = null
+    ;
     function show ($el) { $el.addClass("shown"); }
     function hide ($el) { $el.removeClass("shown"); }
     
-    var $curBg;
-    function backgroundify ($el) {
-        if ($curBg) return;
-        $curBg = $("<div></div>")
-                    .css({
-                        width:  $el.width()
-                    ,   height: $el.height()
-                    ,   top:    $el.offset().top
-                    ,   background: "#333"
-                    ,   filter:     "blur(4px)"
-                    })
-        ;
-        $el.before($curBg);
-    }
-    
+    // var $curBg;
+    // function backgroundify ($el) {
+    //     if ($curBg) return;
+    //     $curBg = $("<div></div>")
+    //                 .css({
+    //                     width:  $el.width()
+    //                 ,   height: $el.height()
+    //                 ,   top:    $el.offset().top
+    //                 ,   background: "#333"
+    //                 ,   filter:     "blur(4px)"
+    //                 })
+    //     ;
+    //     $el.before($curBg);
+    // }
+
+    // XXX debounce?
     function sticky () {
-        $("main, section").each(function() {
-            var $el = $(this)
-            ,   offset = $el.offset()
-            ,   stickScroll = 0+$el.attr("data-sticky-scroll") || 0
-            ,   top = $(window).scrollTop()
-            ,   $floatingHeader = $(".floatingHeader", this)
-            ,   $fluidHeader = $(".fluidHeader", this)
-            ;
-            if ((top > offset.top - stickScroll) && (top < offset.top + $el.height())) {
-                show($floatingHeader);
-                backgroundify($floatingHeader);
-                hide($fluidHeader);
+        var top = $win.scrollTop();
+        // do nothing before first header
+        if (top - headers[0].stickScroll < headers[0].offset) {
+            currentIdx = null;
+            return;
+        }
+        for (var i = 0, n = headers.length; i < n; i++) {
+            var h = headers[i];
+            // XXX we somehow need to target the item *before* the one found
+            if (h.offset >= top) {
+                if (i === currentIdx) return; // the header hasn't changed
+                if (currentIdx !== null) {
+                    hide(headers[currentIdx].floating);
+                    show(headers[currentIdx].fluid);
+                }
+                show(h.floating);
+                hide(h.fluid);
+                currentIdx = i;
+                break;
             }
-            else {
-                hide($floatingHeader);
-                $curBg.remove();
-                show($fluidHeader);
-            }
-       });
+        }
+       //  $("main, section").each(function() {
+       //      var $el = $(this)
+       //      ,   offset = $el.offset()
+       //      ,   stickScroll = 0+$el.attr("data-sticky-scroll") || 0
+       //      ,   top = $(window).scrollTop()
+       //      ,   $floatingHeader = $(".floatingHeader", this)
+       //      ,   $fluidHeader = $(".fluidHeader", this)
+       //      ;
+       //      if ((top > offset.top - stickScroll) && (top < offset.top + $el.height())) {
+       //          show($floatingHeader);
+       //          // backgroundify($floatingHeader);
+       //          hide($fluidHeader);
+       //      }
+       //      else {
+       //          hide($floatingHeader);
+       //          // $curBg.remove();
+       //          show($fluidHeader);
+       //      }
+       // });
     }
 
     $(function() {
         $("main, section").each(function() {
             var $el = $(this)
             ,   $h = $el.find("> :header").first()
-            ,   $clone = $h.clone()
-            ,   stickScroll = $h.attr("data-sticky-scroll") || "0"
+            ,   $clone = $h.clone().removeAttr("id")
+            ,   stickScroll = 0+$h.attr("data-sticky-scroll") || 0
             ;
+            headers.push({
+                offset:         $h.offset().top - stickScroll
+            ,   floating:       $h
+            ,   fluid:          $clone
+            ,   stickScroll:    stickScroll
+            });
             $h
                 .before($clone)
                 .css({
