e93205e9f1ff4dac89c9e4dc5dfe1afe2fd1cc3a
Robin Berjon <robin@berjon.com>
2015-03-11 12:26:31 +0100

    fully functional sticky scrolling

--- js/sticky.js
+++ js/sticky.js
@@ -1,19 +1,4 @@
 
-// XXX
-//  - we need to rethink this.
-//  - currently this applies to all headers, which is inconvenient
-//  - detect passage across top
-//  - linked list of headers
-//  - manage a single floating instance, not lots of clones?
-//  - manage a single background for each
-
-// HOW:
-//  - on load, memorise all header offsets
-//  - on scroll, detect between which two we are
-//  - depending on position, show/hide
-//  - the background thing is handled by *constantly* having said bg element at the top and just
-//    resizing it to match our needs
-
 (function ($) {
     var headers = []
     ,   $win = $(window)
@@ -22,63 +7,33 @@
     function show ($el) { $el.addClass("shown"); }
     function hide ($el) { $el.removeClass("shown"); }
     
-    // var $curBg;
-    // function backgroundify ($el) {
-    //     if ($curBg) return;
-    //     $curBg = $("<div></div>")
-    //                 .css({
-    //                     width:  $el.width()
-    //                 ,   height: $el.height()
-    //                 ,   top:    $el.offset().top
-    //                 ,   background: "#333"
-    //                 ,   filter:     "blur(4px)"
-    //                 })
-    //     ;
-    //     $el.before($curBg);
-    // }
 
     // XXX debounce?
     function sticky () {
         var top = $win.scrollTop();
-        // do nothing before first header
-        if (top - headers[0].stickScroll < headers[0].offset) {
+        if (top - headers[0].margin < headers[0].offset) {
+            if (currentIdx !== null) {
+                hide(headers[currentIdx].floating);
+                show(headers[currentIdx].fluid);
+            }
             currentIdx = null;
             return;
         }
         for (var i = 0, n = headers.length; i < n; i++) {
             var h = headers[i];
-            // XXX we somehow need to target the item *before* the one found
             if (h.offset >= top) {
-                if (i === currentIdx) return; // the header hasn't changed
+                if (i - 1 === currentIdx) return; // the header hasn't changed
                 if (currentIdx !== null) {
                     hide(headers[currentIdx].floating);
                     show(headers[currentIdx].fluid);
                 }
-                show(h.floating);
-                hide(h.fluid);
-                currentIdx = i;
+                currentIdx = i - 1;
+                if (i === 0) return; // not sure about this
+                show(headers[currentIdx].floating);
+                hide(headers[currentIdx].fluid);
                 break;
             }
         }
-       //  $("main, section").each(function() {
-       //      var $el = $(this)
-       //      ,   offset = $el.offset()
-       //      ,   stickScroll = 0+$el.attr("data-sticky-scroll") || 0
-       //      ,   top = $(window).scrollTop()
-       //      ,   $floatingHeader = $(".floatingHeader", this)
-       //      ,   $fluidHeader = $(".fluidHeader", this)
-       //      ;
-       //      if ((top > offset.top - stickScroll) && (top < offset.top + $el.height())) {
-       //          show($floatingHeader);
-       //          // backgroundify($floatingHeader);
-       //          hide($fluidHeader);
-       //      }
-       //      else {
-       //          hide($floatingHeader);
-       //          // $curBg.remove();
-       //          show($fluidHeader);
-       //      }
-       // });
     }
 
     $(function() {
@@ -86,27 +41,30 @@
             var $el = $(this)
             ,   $h = $el.find("> :header").first()
             ,   $clone = $h.clone().removeAttr("id")
-            ,   stickScroll = 0+$h.attr("data-sticky-scroll") || 0
+            ,   margin = parseFloat($h.css("margin-top"), 10)
             ;
             headers.push({
-                offset:         $h.offset().top - stickScroll
+                offset:         $h.offset().top - margin
             ,   floating:       $h
             ,   fluid:          $clone
-            ,   stickScroll:    stickScroll
+            ,   margin:         margin
             });
             $h
                 .before($clone)
                 .css({
                     width:  $h.width()
-                ,   top:    "-" + stickScroll + "px"
+                ,   top:    "-" + margin + "px"
                 })
                 .addClass("floatingHeader")
             ;
             $clone.addClass("fluidHeader");
         });
-        $(window)
-            .scroll(sticky)
-            .trigger("scroll")
-        ;
+
+        $win.scroll(sticky).trigger("scroll");
+        $win.resize(function () {
+            $.each(headers, function (_, h) {
+                h.offset = h.fluid.offset().top - h.margin;
+            });
+        });
     });
 }(jQuery));
